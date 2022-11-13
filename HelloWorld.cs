6392851f5e70fcefcabd1cf17f732da121a23824
SSK0365 <118106148+SSK0365@users.noreply.github.com>
2022-11-13 15:41:00 +0500

    Add files via upload

---
+++ HelloWorld.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelloWorld : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //вывод в консоль надпись, привет мир.
+        print("Hello, World");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+
+
+         }
+
+
+}
